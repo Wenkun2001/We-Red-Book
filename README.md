5f9b6788c36f87df5091999fcea0727f897e48a5
lwk <2470174271@qq.com>
2025-01-21 20:13:02 +0800

    gorm,login,password encryption ,refactor the code,Session,refresh expiration time.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 golang微红书 We-Red-Book 
+
+后端：Gin + Gorm + JWT + Redis + MySQL + k8s + SSO + MongoDB + Kafka + Prometheus + OpenTelemetry + gRPC + etcd + ElasticSearch + and so on
+
+伪前端：React + Next.js + Typescript + axios
+/users/login
---
+++ webook/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/Wenkun2001/We-Red-Book/webook/internal/repository"
+	"github.com/Wenkun2001/We-Red-Book/webook/internal/repository/dao"
+	"github.com/Wenkun2001/We-Red-Book/webook/internal/service"
+	"github.com/Wenkun2001/We-Red-Book/webook/internal/web"
+	"github.com/Wenkun2001/We-Red-Book/webook/internal/web/middleware"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/redis"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"strings"
+	"time"
+)
+
+func main() {
+	db := initDB()
+	server := initWebServer()
+	initUserHdl(db, server)
+	server.Run(":8090")
+
+}
+
+func initUserHdl(db *gorm.DB, server *gin.Engine) {
+	ud := dao.NewUserDAO(db)
+	ur := repository.NewUserRepository(ud)
+	us := service.NewUserService(ur)
+	hdl := web.NewUserHandler(us)
+	hdl.RegisterRoutes(server)
+}
+
+func initDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:13316)/webook"))
+	if err != nil {
+		panic(err)
+	}
+	err = dao.InitTables(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initWebServer() *gin.Engine {
+	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		//AllowAllOrigins: true,
+		//AllowOrigins:     []string{"http://localhost:3000"},
+		AllowCredentials: true,
+
+		AllowHeaders: []string{"Content-Type"},
+		//AllowHeaders: []string{"content-type"},
+		//AllowMethods: []string{"POST"},
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				//if strings.Contains(origin, "localhost") {
+				return true
+			}
+			return strings.Contains(origin, "your_company.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}), func(ctx *gin.Context) {
+		println("这是我的 Middleware")
+	})
+	login := &middleware.LoginMiddlewareBuilder{}
+	// 存储数据的，也就是你 userId 存哪里
+	// 直接存 cookie
+	//store := cookie.NewStore([]byte("secret"))
+	// 基于内存的实现
+	//store := memstore.NewStore([]byte("k6CswdUm75WKcbM68UQUuxVsHSpTCwgK"),
+	//	[]byte("eF1`yQ9>yT1`tH1,sJ0.zD8;mZ9~nC6("))
+	store, err := redis.NewStore(16, "tcp",
+		"localhost:6379", "",
+		[]byte("k6CswdUm75WKcbM68UQUuxVsHSpTCwgK"),
+		[]byte("k6CswdUm75WKcbM68UQUuxVsHSpTCwgA"))
+	if err != nil {
+		panic(err)
+	}
+	server.Use(sessions.Sessions("ssid", store), login.CheckLogin())
+	return server
+
+	return server
+}
