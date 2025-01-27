6ecd6e0ecf89072d44d716c11ac3edb761d7c522
lwk <2470174271@qq.com>
2025-01-27 10:40:56 +0800

    Configuration module.

--- webook/ioc/db.go
+++ webook/ioc/db.go
@@ -1,14 +1,24 @@
 package ioc
 
 import (
-	"github.com/Wenkun2001/We-Red-Book/webook/config"
 	"github.com/Wenkun2001/We-Red-Book/webook/internal/repository/dao"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
+	type Config struct {
+		DSN string `yaml:"dsn"`
+	}
+	var cfg Config = Config{
+		DSN: "root:root@tcp(localhost:3316)/webook",
+	}
+	err := viper.UnmarshalKey("db", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(mysql.Open(cfg.DSN))
 	if err != nil {
 		panic(err)
 	}
